f516d3699feca7808fcd6c6f267981b168093dfa
Carlos A. <carlosvsk@gmail.com>
2025-05-23 12:20:40 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -197,9 +197,17 @@ zIndex: "-1px",
 'lst2': [{name: "Tarefa 1", date: "21/03/25"},{name: "Tarefa 2", date: "21/03/25"},{name: "Tarefa 3", date: "24/03/25"}], 'lst3': [{name: "Task 1", check: false},{name: "Task 2", check: false}] } 
 , 
 'name': "Name:", 
-'dataToSet': {name: "Carlos", phone: "1111"}, 'toggles': { 
+'dataToSet': {name: "Carlos", phone: "1111"}, 
+'toggles': { 
 'box1': true, 'checkbox1': false } 
- } 
+, 'firebaseConfig ': {
+  apiKey: "AIzaSyDjAfyEUADq7EaRyFWlGFjP1Eoox9LJHgI",
+  authDomain: "devs-tests-95208.firebaseapp.com",
+  projectId: "devs-tests-95208",
+  storageBucket: "devs-tests-95208.appspot.com",
+  messagingSenderId: "750912250366",
+  appId: "1:750912250366:web:4629eac789a718a74220af"
+} } 
  
 } 
  );
