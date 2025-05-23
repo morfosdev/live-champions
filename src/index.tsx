5ee35d94e57fa50773e0e81bba12e5257282cc87
Carlos A. <carlosvsk@gmail.com>
2025-05-23 17:03:29 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -473,27 +473,7 @@ zIndex: "-1px",
  arrFunctions: [async (...args) =>
         functions.firebase.getDocsTool({ args, pass:{
    arrRefStrings: [`championsScores`],
-            arrFuncs: [(args) => {
- console.log({ args });
- const path1 = "sc.A0.lists.list1";
- const path2 = "sc.A0.result";
-
-  const vitorias = { psg: 0, inter: 0, empate: 0 };
-args.forEach(p => {
-  const psgGols = parseInt(p.psg);
-  const interGols = parseInt(p.inter);
-
-  if (psgGols > interGols) vitorias.psg++;
-    else if (interGols > psgGols) vitorias.inter++;
-    else vitorias.empate++;
-});
-
- const pass1 = { keyPath: [path1], value: [args] };
- tools.functions.setVar({ args: "", pass1 });
-
- const pass2 = { keyPath: [path2], value: [vitorias] };
- tools.functions.setVar({ args: "", pass2 });
-}],
+            arrFuncs: [undefined],
         }})]
  , trigger: 'on press'
 }})],            childrenItems:[(...args:any) => <Elements.Text pass={{
