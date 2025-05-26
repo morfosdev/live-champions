9008e20b786195492768c97d0a370739881aab5c
Leandro <leandrowebmaster@gmail.com>
2025-05-26 10:11:46 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -378,7 +378,7 @@ top: "280px",
             elementsProperties:['{}'],
 
             styles:[`{
-width: "100%",
+width: "300px",
 flexDirection: "row",
 backgroundColor: "#CCC",
 borderRadius: "15px",
